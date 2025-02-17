c0418e89f8682a8d545b2b02a23a02f8706e1cd9
Yuan SUN <sunyuan860510@gmail.com>
2025-02-17 23:37:38 +0100

    add pypi package action workflow

--- src/lint.py
+++ src/lint.py
@@ -1,9 +1,8 @@
-import argparse
 import pathlib
-import sys
 
-from utils import exec_on_files, pasre_ignore_dirs, print_exec_info, read_ignore_paths
 from utils import DEFAULT_IGNORE_DIRS
+from utils import (exec_on_files, pasre_ignore_dirs, print_exec_info,
+                   read_ignore_paths)
 
 
 def lint_code(args):
@@ -73,71 +72,3 @@ def lint_code(args):
                                                     verbose=args.verbose,
                                                     dry_run=args.dry_run)
         print_exec_info('flake8', num_failed, failed_commands)
-
-
-def parse_command_line(argv):
-    parser = argparse.ArgumentParser(
-        description='lint the code.',
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-
-    required_args = parser.add_argument_group('required arguments')
-    required_args.add_argument(
-        '-p',
-        '--project-root-dir',
-        required=True,
-        type=pathlib.Path,
-        help='Path to your project root directory folder.')
-
-    optional_args = parser.add_argument_group('optional arguments')
-
-    optional_args.add_argument('-l',
-                               '--languages',
-                               choices=['cxx', 'python', 'cmake'],
-                               nargs="+",
-                               default=['cxx', 'python', 'cmake'],
-                               help='Which languages to lint')
-    optional_args.add_argument(
-        '-id',
-        '--ignore-dirs',
-        nargs="+",
-        type=pathlib.Path,
-        help='Path of directories where formater ignores')
-
-    optional_args.add_argument(
-        '-if',
-        '--ignore-file',
-        type=pathlib.Path,
-        help='Path of directories where formater ignores')
-
-    optional_args.add_argument('-v',
-                               '--verbose',
-                               action='store_true',
-                               help='enable verbose mode')
-    optional_args.add_argument('-d',
-                               '--dry-run',
-                               action='store_true',
-                               help='enable dry-run mode')
-
-    cmake_args = parser.add_argument_group('optional cmake lint arguments')
-    cmake_args.add_argument('--cmake-lint-config',
-                            type=pathlib.Path,
-                            help='Path of cmake lint custom config file')
-    cxx_args = parser.add_argument_group('optional c++ format arguments')
-    cxx_args.add_argument('--clang-tidy-config',
-                          type=pathlib.Path,
-                          help='Path of clang tidy custom config file')
-    python_args = parser.add_argument_group('optional python format arguments')
-    python_args.add_argument('--flake8-config',
-                             type=pathlib.Path,
-                             help='Path of flake8 custom config file')
-
-    return parser.parse_args(argv)
-
-
-def main():
-    args = parse_command_line(sys.argv[1:])
-    lint_code(args)
-
-
-if __name__ == '__main__':
-    main()
